3d86fffbd523b7e0c486c6295bdbb561e9fcd48a
MyoKO (Nick) <42021479+myoko1996@users.noreply.github.com>
2025-12-27 18:28:27 +0630

    preparing-Zonal-Resiliency- 3 tier architecure

---
+++ terraform.tfvars
@@ -0,0 +1,7 @@
+application_name = "fleet-ops"
+environment_name = "dev"
+primary_region   = "westus2"
+
+
+vnet_cidr_block       = "10.0.0.0/22"
+az_count              = 2
---
+++ variables.tf
@@ -0,0 +1,16 @@
+variable "application_name" {
+  type = string
+}
+variable "environment_name" {
+  type = string
+}
+variable "primary_region" {
+  type = string
+}
+
+variable "vnet_cidr_block" {
+  type = string
+}
+variable "az_count" {
+  type = number
+}
